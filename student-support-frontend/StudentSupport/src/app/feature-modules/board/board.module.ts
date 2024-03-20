ab5ed1a72257445588a54543a5568b203d6ef015
JovanSarac <jovan_bn@outlook.com>
2024-03-21 00:04:54 +0100

    feat:Add board when user login

--- student-support-frontend/StudentSupport/src/app/feature-modules/board/board.module.ts
+++ student-support-frontend/StudentSupport/src/app/feature-modules/board/board.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InformationBoardComponent } from './information-board/information-board.component';
+import { LayoutModule } from '../layout/layout.module';
 
 
 
@@ -9,7 +10,8 @@ import { InformationBoardComponent } from './information-board/information-board
     InformationBoardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    LayoutModule
   ],
   exports:[
     InformationBoardComponent
