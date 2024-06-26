a98d8d0e6966712c2ad1b15cf81fe47256e58a76
JovanSarac <jovan_bn@outlook.com>
2024-06-26 02:19:21 +0200

    feat:create pagination for event and create tab for all type of events

--- student-support-frontend/StudentSupport/src/app/feature-modules/layout/home/home.component.ts
+++ student-support-frontend/StudentSupport/src/app/feature-modules/layout/home/home.component.ts
@@ -1,17 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MyEvent } from '../../board/model/myevent.model';
+import { LayoutService } from '../layout.service';
+import { PagedResults } from 'src/app/shared/model/paged-results.model';
+
 
 @Component({
   selector: 'xp-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  logincomponent : boolean = false;
-
+export class HomeComponent  implements OnInit{
   constructor(
-    private router: Router
+    private router: Router,
+    private service : LayoutService,
   ) {}
+
+  selectedTab: string = 'all';
+  events : MyEvent[] =  [];
+  currentPage = 1;
+  pageSize = 12;
+  pagedEvents: MyEvent[] = [];
+  totalPages = 1;
+
+  ngOnInit() {
+    this.service.getAllEvenets().subscribe({
+      next: (result:PagedResults<MyEvent>)=>{
+        this.events = result.results;
+        this.totalPages = Math.ceil(this.events.length / this.pageSize);
+        this.updatePagedEvents();
+      }
+    })
+  
+  }
+
+  selectTab(tab: string) {
+    this.selectedTab = tab;
+    this.currentPage = 1; // Reset page to 1 when tab is selected
+    this.updatePagedEvents();
+  }
+
+  updatePagedEvents() {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.pagedEvents = this.events.slice(startIndex, endIndex);
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updatePagedEvents();
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePagedEvents();
+    }
+  }
+
 
 
   loginClick() : void{
---
+++ student-support-frontend/StudentSupport/src/app/feature-modules/layout/layout.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PagedResults } from 'src/app/shared/model/paged-results.model';
+import { MyEvent } from '../board/model/myevent.model';
+import { environment } from 'src/env/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LayoutService {
+
+  constructor(private http: HttpClient) { }
+
+
+  getAllEvenets(): Observable<PagedResults<MyEvent>> {
+    return this.http.get<PagedResults<MyEvent>>(environment.apiHost + 'author/events');
+}
+}
