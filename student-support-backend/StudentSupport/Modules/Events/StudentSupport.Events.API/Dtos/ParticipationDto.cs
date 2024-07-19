f120814ac7d1457bca8f317659e0c776b2cdf8f6
Jovan Jokić <jovanjokic555@gmail.com>
2024-07-19 02:20:18 +0200

    feat: Create Participation DTO

---
+++ student-support-backend/StudentSupport/Modules/Events/StudentSupport.Events.API/Dtos/ParticipationDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSupport.Events.API.Dtos
+{
+    public class ParticipationDto
+    {
+        public long Id { get; set; }
+        public long EventId { get; set; }
+        public long StudentId { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public string Type { get; set; }
+    }
+}
