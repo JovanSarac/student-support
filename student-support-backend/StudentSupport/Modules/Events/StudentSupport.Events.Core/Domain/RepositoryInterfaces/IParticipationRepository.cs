2fa6cdc382f3bd0d75c1811e2fe87eded645a071
Jovan Jokić <jovanjokic555@gmail.com>
2024-07-19 02:20:56 +0200

    feat: Create Participation Repository and all required methods

---
+++ student-support-backend/StudentSupport/Modules/Events/StudentSupport.Events.Core/Domain/RepositoryInterfaces/IParticipationRepository.cs
@@ -0,0 +1,13 @@
+﻿using StudentSupport.BuildingBlocks.Core.UseCases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSupport.Events.Core.Domain.RepositoryInterfaces
+{
+    public interface IParticipationRepository : ICrudRepository<Participation>
+    {
+    }
+}
---
+++ student-support-backend/StudentSupport/Modules/Events/StudentSupport.Events.Infrastructure/Database/Repositories/ParticipationDatabaseRepository.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentSupport.BuildingBlocks.Core.UseCases;
+using StudentSupport.BuildingBlocks.Infrastructure.Database;
+using StudentSupport.Events.Core.Domain;
+using StudentSupport.Events.Core.Domain.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSupport.Events.Infrastructure.Database.Repositories
+{
+    public class ParticipationDatabaseRepository : IParticipationRepository
+    {
+        private readonly EventsContext _eventsContext;
+        private readonly DbSet<Participation> _participations;
+
+        public ParticipationDatabaseRepository(EventsContext eventsContext)
+        {
+            _eventsContext = eventsContext;
+            _participations = _eventsContext.Set<Participation>();
+        }
+
+        public Participation Create(Participation entity)
+        {
+            _participations.Add(entity);
+            _eventsContext.SaveChanges();
+            return entity;
+        }
+
+        public void Delete(long id)
+        {
+            var entity = Get(id);
+            _participations.Remove(entity);
+            _eventsContext.SaveChanges();
+        }
+
+        public Participation Get(long id)
+        {
+            var participation = _participations
+                .Where(p => p.Id == id)
+                .FirstOrDefault();
+            return participation ?? throw new KeyNotFoundException("Not found: " + id);
+        }
+
+        public PagedResult<Participation> GetPaged(int page, int pageSize)
+        {
+            var task = _participations.GetPagedById(page, pageSize);
+            task.Wait();
+            return task.Result;
+        }
+
+        public Participation Update(Participation entity)
+        {
+            try
+            {
+                _eventsContext.Update(entity);
+                _eventsContext.SaveChanges();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new KeyNotFoundException(e.Message);
+            }
+            return entity;
+        }
+
+        public void SaveChanges()
+        {
+            _eventsContext.SaveChanges();
+        }
+    }
+}
