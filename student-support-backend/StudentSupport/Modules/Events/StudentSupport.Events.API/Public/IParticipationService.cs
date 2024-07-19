d1b5655080fb000ad8477ebe2314e0dea321d5c7
Jovan Jokić <jovanjokic555@gmail.com>
2024-07-19 02:21:31 +0200

    feat: Create Participation service and all required methods

---
+++ student-support-backend/StudentSupport/Modules/Events/StudentSupport.Events.API/Public/IParticipationService.cs
@@ -0,0 +1,20 @@
+﻿using FluentResults;
+using StudentSupport.BuildingBlocks.Core.UseCases;
+using StudentSupport.Events.API.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSupport.Events.API.Public
+{
+    public interface IParticipationService
+    {
+        Result<PagedResult<ParticipationDto>> GetPaged(int page, int pageSize);
+        Result<ParticipationDto> Get(int id);
+        Result<ParticipationDto> Create(ParticipationDto participationDto);
+        Result<ParticipationDto> Update(ParticipationDto participationDto);
+        Result Delete(int id);
+    }
+}
---
+++ student-support-backend/StudentSupport/Modules/Events/StudentSupport.Events.Core/UseCases/ParticipationService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using StudentSupport.BuildingBlocks.Core.UseCases;
+using StudentSupport.Events.API.Dtos;
+using StudentSupport.Events.API.Public;
+using StudentSupport.Events.Core.Domain;
+using StudentSupport.Events.Core.Domain.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSupport.Events.Core.UseCases
+{
+    public class ParticipationService : CrudService<ParticipationDto, Participation>, IParticipationService
+    {
+        private readonly IParticipationRepository _participationRepository;
+        private readonly IMapper _mapper;
+
+        public ParticipationService(IParticipationRepository participationRepository, IMapper mapper) : base(participationRepository, mapper) 
+        {
+            _participationRepository = participationRepository;
+            _mapper = mapper;
+        }
+
+
+    }
+}
