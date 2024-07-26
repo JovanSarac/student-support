b9f852f76252e1a8b18aaaf770f3f07720e29d7a
Jovan Jokić <jovanjokic555@gmail.com>
2024-07-26 20:24:06 +0200

    feat: Complete single event page view

--- student-support-frontend/StudentSupport/src/app/feature-modules/events/single-event-page/single-event-page.component.ts
+++ student-support-frontend/StudentSupport/src/app/feature-modules/events/single-event-page/single-event-page.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MyEvent } from '../../board/model/myevent.model';
 import { EventsService } from '../events.service';
+import { Person } from 'src/app/shared/model/person.model';
+import { MapComponent } from 'src/app/shared/map/map.component';
+import { User } from 'src/app/infrastructure/auth/model/user.model';
+import { AuthService } from 'src/app/infrastructure/auth/auth.service';
+import {
+  Participation,
+  ParticipationType,
+} from 'src/app/shared/model/participation-model';
 
 @Component({
   selector: 'app-single-event-page',
@@ -9,7 +17,22 @@ import { EventsService } from '../events.service';
   styleUrls: ['./single-event-page.component.css'],
 })
 export class SingleEventPageComponent implements OnInit {
+  @ViewChild(MapComponent) mapComponent!: MapComponent;
   eventId: number = 0;
+  participationNumber: number = 0;
+  isLoading: boolean = false;
+  eventIdForLoader: number = 0;
+  user!: User;
+  participations: Participation[] = [];
+
+  participation: Participation = {
+    id: 0,
+    eventId: 0,
+    studentId: 0,
+    enrollmentDate: new Date(),
+    type: ParticipationType.Active,
+  };
+
   event: MyEvent = {
     id: 0,
     name: '',
@@ -24,19 +47,127 @@ export class SingleEventPageComponent implements OnInit {
     longitude: 0,
   };
 
-  constructor(private route: ActivatedRoute, private service: EventsService) {}
+  author: Person = {
+    id: 0,
+    name: '',
+    surname: '',
+    userId: 0,
+    email: '',
+    profilePic: '',
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private service: EventsService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.eventId = Number(this.route.snapshot.paramMap.get('eventId'));
+    this.getLoggedUser();
     this.getEventById();
+  }
+
+  setMarkerOnMap(): void {
+    if (this.mapComponent) {
+      this.mapComponent.addMarker(this.event.latitude, this.event.longitude);
+      this.mapComponent.setView(this.event.latitude, this.event.longitude, 15);
+    }
+  }
+
+  getLoggedUser(): void {
+    this.authService.user$.subscribe((user) => {
+      this.user = user;
+      this.getParticipationsByStudentId();
+    });
+  }
+
+  participateEvent(eventId: number): void {
+    this.isLoading = true;
+    this.eventIdForLoader = eventId;
+    this.participation.eventId = eventId;
+    this.participation.studentId = this.user.id;
+    this.service.participateEvent(this.participation).subscribe({
+      next: (result: Participation) => {
+        this.isLoading = false;
+        this.eventIdForLoader = 0;
+        this.getParticipationsByStudentId();
+        this.countParticipationsByEventId();
+      },
+    });
+  }
+
+  cancelEventParticipation(eventId: number): void {
+    const cancelledParticipation = this.participations.find(
+      (p) =>
+        p.studentId === this.user.id &&
+        p.eventId === eventId &&
+        p.type === ParticipationType.Active
+    );
+
+    this.service.cancelParticipation(cancelledParticipation?.id!).subscribe({
+      next: (result: Participation) => {
+        this.countParticipationsByEventId();
+        this.getParticipationsByStudentId();
+      },
+    });
+  }
+
+  getParticipationsByStudentId(): void {
+    this.service.getAllParticipationsByStudentId(this.user.id).subscribe({
+      next: (result: Participation[]) => {
+        this.participations = result;
+      },
+    });
+  }
+
+  isAlreadyCheckedParticipate(eventId: number): boolean {
+    return this.participations.some(
+      (p) => p.eventId === eventId && p.type === ParticipationType.Active
+    );
+  }
+
+  shouldShowLoader(eventId: number): boolean {
+    if (this.eventIdForLoader === eventId) {
+      return true;
+    }
+    return false;
   }
 
   getEventById(): void {
     this.service.getEventById(this.eventId).subscribe({
       next: (result: MyEvent) => {
         this.event = result;
-        console.log(this.event);
+        this.getAuthor();
+        this.countParticipationsByEventId();
+        this.setMarkerOnMap();
       },
     });
+  }
+
+  getAuthor(): void {
+    this.service.getPersonById(this.event.userId).subscribe({
+      next: (result: Person) => {
+        this.author = result;
+      },
+    });
+  }
+
+  countParticipationsByEventId(): void {
+    this.service.countParticipationsByEventId(this.event.id).subscribe({
+      next: (result: number) => {
+        this.participationNumber = result;
+      },
+    });
+  }
+
+  formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    const day = ('0' + date.getDate()).slice(-2);
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const year = date.getFullYear();
+    const hours = ('0' + date.getHours()).slice(-2);
+    const minutes = ('0' + date.getMinutes()).slice(-2);
+    return `${day}.${month}.${year} ${hours}:${minutes}`;
   }
 }
