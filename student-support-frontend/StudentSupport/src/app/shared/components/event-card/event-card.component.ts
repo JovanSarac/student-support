a06467ba124a4195630e3b2318059e758611b6ee
Jovan Jokić <jovanjokic555@gmail.com>
2024-08-03 19:57:18 +0200

    feat: Create reusable custom event card component

---
+++ student-support-frontend/StudentSupport/src/app/shared/components/event-card/event-card.component.ts
@@ -0,0 +1,157 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { MyEvent } from 'src/app/feature-modules/board/model/myevent.model';
+import { User } from 'src/app/infrastructure/auth/model/user.model';
+import {
+  Participation,
+  ParticipationType,
+} from '../../model/participation-model';
+import { EventsService } from 'src/app/feature-modules/events/events.service';
+import { AuthService } from 'src/app/infrastructure/auth/auth.service';
+
+@Component({
+  selector: 'xp-event-card',
+  templateUrl: './event-card.component.html',
+  styleUrls: ['./event-card.component.css'],
+})
+export class EventCardComponent implements OnInit {
+  @Input() event: MyEvent = {
+    id: 10,
+    address: 'Novi Sad, Lasla Gala 28a',
+    dateEvent: new Date(),
+    datePublication: new Date(),
+    description: '',
+    eventType: 'ArtExhibitionsAndPerformances',
+    image:
+      'https://c4.wallpaperflare.com/wallpaper/556/382/458/fantasy-art-artwork-fan-art-science-fiction-wallpaper-preview.jpg',
+    isArchived: false,
+    latitude: 0,
+    longitude: 0,
+    name: 'Art&Science Event - Spojimo nauku i umetnost kako to najbolje umemo',
+    userId: 0,
+  };
+  @Output() OnClick = new EventEmitter();
+  user!: User;
+  participation: Participation = {
+    id: 0,
+    eventId: 0,
+    studentId: 0,
+    enrollmentDate: new Date(),
+    type: ParticipationType.Active,
+  };
+  participations: Participation[] = [];
+  isLoading: boolean = false;
+  eventIdForLoader: number = 0;
+
+  constructor(
+    public router: Router,
+    private service: EventsService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.getLoggedUser();
+    this.getParticipationsByStudentId();
+  }
+
+  emitEvent(): void {
+    this.OnClick.emit();
+  }
+
+  showSingleEvent(): void {
+    this.router.navigate([`/single-event/${this.event.id}`]);
+  }
+
+  getParticipationsByStudentId(): void {
+    this.service.getAllParticipationsByStudentId(this.user.id).subscribe({
+      next: (result: Participation[]) => {
+        this.participations = result;
+      },
+    });
+  }
+
+  participateEvent(event: Event, eventId: number): void {
+    event.stopPropagation();
+    this.isLoading = true;
+    this.eventIdForLoader = eventId;
+    this.participation.eventId = eventId;
+    this.participation.studentId = this.user.id;
+    this.service.participateEvent(this.participation).subscribe({
+      next: (result: Participation) => {
+        this.getParticipationsByStudentId();
+        this.isLoading = false;
+        this.eventIdForLoader = 0;
+      },
+    });
+  }
+
+  cancelEventParticipation(event: Event, eventId: number): void {
+    event.stopPropagation();
+    const cancelledParticipation = this.participations.find(
+      (p) =>
+        p.studentId === this.user.id &&
+        p.eventId === eventId &&
+        p.type === ParticipationType.Active
+    );
+
+    this.service.cancelParticipation(cancelledParticipation?.id!).subscribe({
+      next: (result: Participation) => {
+        this.getParticipationsByStudentId();
+      },
+    });
+  }
+
+  shouldShowLoader(eventId: number): boolean {
+    if (this.eventIdForLoader === eventId) {
+      return true;
+    }
+    return false;
+  }
+
+  isAlreadyCheckedParticipate(eventId: number): boolean {
+    return this.participations.some(
+      (p) => p.eventId === eventId && p.type === ParticipationType.Active
+    );
+  }
+
+  getLoggedUser(): void {
+    this.authService.user$.subscribe((user) => {
+      this.user = user;
+      if (user.role === 'student') this.getParticipationsByStudentId();
+    });
+  }
+
+  formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    const day = ('0' + date.getDate()).slice(-2);
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const year = date.getFullYear();
+    const hours = ('0' + date.getHours()).slice(-2);
+    const minutes = ('0' + date.getMinutes()).slice(-2);
+    return `${day}.${month}.${year} ${hours}:${minutes}`;
+  }
+
+  eventType: { [key: string]: string } = {
+    AcademicConferenceAndSeminars: 'Konferencije',
+    WorkshopsAndCourses: 'Kursevi',
+    CulturalEvent: 'Kulturni',
+    Fair: 'Sajamski',
+    HumanitarianEvent: 'Humanitarni',
+    ArtExhibitionsAndPerformances: 'Umjetnicki',
+    StudentPartiesAndSocialEvents: 'Drustveni',
+    Competitions: 'Takmicenja',
+    StudentTrips: 'Putovanja',
+  };
+
+  eventTypeColors: { [key: string]: string } = {
+    AcademicConferenceAndSeminars: '#429D66',
+    WorkshopsAndCourses: ' #FF4D4D',
+    CulturalEvent: '#00BFFF',
+    Fair: '#FF9501',
+    HumanitarianEvent: '#FFD700',
+    ArtExhibitionsAndPerformances: '#DF80FF',
+    StudentPartiesAndSocialEvents: '#66CDAA',
+    Competitions: '#FFA07A',
+    StudentTrips: 'rgb(61,61,254)',
+  };
+}
--- student-support-frontend/StudentSupport/src/app/shared/shared.module.ts
+++ student-support-frontend/StudentSupport/src/app/shared/shared.module.ts
@@ -4,14 +4,21 @@ import { MapComponent } from './map/map.component';
 import { PrimaryButtonComponent } from './components/primary-button/primary-button.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SecondaryButtonComponent } from './components/secondary-button/secondary-button.component';
+import { EventCardComponent } from './components/event-card/event-card.component';
 
 @NgModule({
   declarations: [
     MapComponent,
     PrimaryButtonComponent,
     SecondaryButtonComponent,
+    EventCardComponent,
   ],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [MapComponent, PrimaryButtonComponent, SecondaryButtonComponent],
+  exports: [
+    MapComponent,
+    PrimaryButtonComponent,
+    SecondaryButtonComponent,
+    EventCardComponent,
+  ],
 })
 export class SharedModule {}
