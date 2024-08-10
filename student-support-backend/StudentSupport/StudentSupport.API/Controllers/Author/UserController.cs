da0594f339d6e3ca5cc53e3f6281c3968ac7a145
Jovan Jokić <jovanjokic555@gmail.com>
2024-08-10 21:44:30 +0200

    feat: Create user controller for author/student

---
+++ student-support-backend/StudentSupport/StudentSupport.API/Controllers/Author/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StudentSupport.Stakeholders.API.Dtos;
+using StudentSupport.Stakeholders.API.Public;
+using StudentSupport.Stakeholders.Core.UseCases;
+
+namespace StudentSupport.API.Controllers.Author
+{
+    [Authorize(Policy = "authorPolicy")]
+    [Route("api/author/users")]
+    public class UserController : BaseApiController
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("{userId:int}")]
+        public ActionResult<UserDto> GetByUserId(int userId)
+        {
+            var result = _userService.GetByUserId(userId);
+            return CreateResponse(result);
+        }
+    }
+}
---
+++ student-support-backend/StudentSupport/StudentSupport.API/Controllers/Student/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StudentSupport.Stakeholders.API.Dtos;
+using StudentSupport.Stakeholders.API.Public;
+
+namespace StudentSupport.API.Controllers.Student
+{
+    [Authorize(Policy = "studentPolicy")]
+    [Route("api/student/users")]
+    public class UserController : BaseApiController
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("{userId:int}")]
+        public ActionResult<UserDto> GetByUserId(int userId)
+        {
+            var result = _userService.GetByUserId(userId);
+            return CreateResponse(result);
+        }
+    }
+}
