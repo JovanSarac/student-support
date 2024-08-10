0908ea9e7df459fbbae79b859842d91c7953215c
Jovan Jokić <jovanjokic555@gmail.com>
2024-08-10 21:43:32 +0200

    feat: Create UserDto

---
+++ student-support-backend/StudentSupport/Modules/Stakeholders/StudentSupport.Stakeholders.API/Dtos/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSupport.Stakeholders.API.Dtos
+{
+    public class UserDto
+    {
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public int Role { get; private set; }
+        public bool IsActive { get; set; }
+        public bool RegisterWithEmail { get; set; }
+    }
+}
