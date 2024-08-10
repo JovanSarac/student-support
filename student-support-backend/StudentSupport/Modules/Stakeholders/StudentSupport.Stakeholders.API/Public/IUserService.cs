4e0068074c78cd2ad2e48e85faa6b31b2697238f
Jovan Jokić <jovanjokic555@gmail.com>
2024-08-10 21:43:58 +0200

    feat: Create User service

---
+++ student-support-backend/StudentSupport/Modules/Stakeholders/StudentSupport.Stakeholders.API/Public/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using FluentResults;
+using StudentSupport.Stakeholders.API.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSupport.Stakeholders.API.Public
+{
+    public interface IUserService
+    {
+        Result<UserDto> GetByUserId(int userId);
+    }
+}
---
+++ student-support-backend/StudentSupport/Modules/Stakeholders/StudentSupport.Stakeholders.Core/UseCases/UserService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using FluentResults;
+using StudentSupport.BuildingBlocks.Core.UseCases;
+using StudentSupport.Stakeholders.API.Dtos;
+using StudentSupport.Stakeholders.API.Public;
+using StudentSupport.Stakeholders.Core.Domain;
+using StudentSupport.Stakeholders.Core.Domain.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSupport.Stakeholders.Core.UseCases
+{
+    public class UserService : BaseService<UserDto, User>, IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository, IMapper mapper) : base(mapper)
+        {
+            _userRepository = userRepository;
+        }
+
+        public Result<UserDto> GetByUserId(int userId)
+        {
+            try
+            {
+                var result = _userRepository.GetById(userId);
+                return MapToDto(result);
+            }
+            catch (KeyNotFoundException e)
+            {
+                return Result.Fail(FailureCode.NotFound).WithError(e.Message);
+            }
+        }
+    }
+}
