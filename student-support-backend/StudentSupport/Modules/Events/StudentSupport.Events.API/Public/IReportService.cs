274bfc0fd309df821bf2a099779f6d4bc386709f
Jovan Jokić <jovanjokic555@gmail.com>
2024-08-14 19:35:11 +0200

    feat: Create Report service

---
+++ student-support-backend/StudentSupport/Modules/Events/StudentSupport.Events.API/Public/IReportService.cs
@@ -0,0 +1,23 @@
+﻿using FluentResults;
+using StudentSupport.BuildingBlocks.Core.UseCases;
+using StudentSupport.Events.API.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSupport.Events.API.Public
+{
+    public interface IReportService
+    {
+        Result<PagedResult<ReportDto>> GetPaged(int page, int pageSize);
+        Result<ReportDto> Get(int id);
+        Result<ReportDto> Create(ReportDto reportDto);
+        Result<ReportDto> Update(ReportDto reportDto);
+        Result Delete(int id);
+        Result<ReportDto> Resolve(int id);
+        Result<ReportDto> Dismiss(int id);
+        Result<ReportDto> Close(int id);
+    }
+}
