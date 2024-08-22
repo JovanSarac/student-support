cb81d3c35a03056dedec43656db1ed2269f20e4f
Jovan Jokić <jovanjokic555@gmail.com>
2024-08-23 00:05:53 +0200

    feat: Create announcementdto

---
+++ student-support-backend/StudentSupport/Modules/Clubs/StudentSupport.Clubs.API/Dtos/AnnouncementDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSupport.Clubs.API.Dtos
+{
+    public class AnnouncementDto
+    {
+        public long Id { get; set; }
+        public long ClubId { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string Content { get; set; }
+        public long AnnouncerId { get; set; }
+        public List<string> Images { get; init; }
+    }
+}
